030cb4b3d88752a534e1d8debfee997ed3db3fba
Alexey Peskov <me@sandlex.com>
2018-08-09 22:41:27 +0200

    App skeleton

---
+++ src/main/java/com/sandlex/xpnsr/XpnsrApplication.java
@@ -0,0 +1,12 @@
+package com.sandlex.xpnsr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XpnsrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XpnsrApplication.class, args);
+    }
+}
